db044523fc66702520eccfe9c57a53cabddad386
merve <mervue4554@gmail.com>
2023-09-16 07:19:04 +0200

    versiyon 3

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class Class01 {
     public static void main(String[] args) {
 
@@ -9,8 +11,12 @@ public class Class01 {
         System.out.println("Version 1 icin islem tamam!");
 
         //version 2 calismasi
-        int z=4
+        int z=4;
        System.out.println(x+y+z);
+       //V3
+        System.out.println("V3 calismalari");
+        //V4
+        System.out.println("V4 kodlari");
 
 
     }
